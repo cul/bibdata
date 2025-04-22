77c0f2cc880165102996905a0d3dea1890c264c6
goldsmithb <bg2918@columbia.edu>
2025-04-22 10:52:42 -0400

    [LIBSYS-7604] add req test

--- spec/requests/barcode_spec.rb
+++ spec/requests/barcode_spec.rb
@@ -2,10 +2,23 @@ require 'rails_helper'
 
 RSpec.describe "Barcodes", type: :request do
   describe "GET /show" do
-    it "returns http success" do
-      get "/barcode/show"
-      expect(response).to have_http_status(:success)
-      puts "huzzah!"
+    let(:test_barcode) { "test" }
+    let(:test_xml) { "<record><title>Test Title</title></record>" }
+
+    it "returns XML and OK response" do
+      allow_any_instance_of(BarcodeController).to receive(:erics_script).with(test_barcode).and_return(test_xml)
+
+      get "/barcode/#{test_barcode}"
+      expect(response).to have_http_status(:ok)
+      expect(response.content_type).to eq("application/xml; charset=utf-8")
+      expect(response.content).to eq(test_xml)
+    end
+
+    it "returns Not Found code with bad request" do
+      allow_any_instance_of(BarcodeController).to receive(:erics_script).with(test_barcode).and_return(nil)
+
+      get "/barcode/#{test_barcode}"
+      expect(response).to have_http_status(:not_found)
     end
   end
 end
--- spec/routing/barcode_spec.rb
+++ spec/routing/barcode_spec.rb
@@ -3,13 +3,11 @@ require 'rails_helper'
 RSpec.describe "Barcodes", type: :routing do
   describe "GET /show" do
     it "returns http success" do
-      puts "victory !!!!"
       expect(get: "/barcode/test").to route_to(
         controller: "barcode",
         action: "show",
         barcode: "test"
       )
-      puts "victory!"
     end
   end
 end
