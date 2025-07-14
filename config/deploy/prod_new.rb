556204fac3b4ae341eb32531bac656247914dec7
Eric O <eric@ejdigitalmedia.com>
2025-07-14 12:24:20 -0400

    Remove no-longer-needed prod_new deploy target

--- config/deploy/prod_new.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-set :remote_user, "renserv"
-
-server "diglib-rails-prod1.cul.columbia.edu", user: fetch(:remote_user), roles: %w[app db web]
-# In test/prod, suggest latest tag as default version to deploy
-ask :branch, `git tag --sort=version:refname`.split("\n").last
